6bfcc96fce2be8148ff4be27a15a9a11c615c74a
Ittichote <wi.ittichote_st@tni.ac.th>
2017-02-03 02:31:03 +0000

    Commit 5 -generate_lsit v2 tested

--- src/generate_list.py
+++ src/generate_list.py
@@ -2,7 +2,7 @@ import random
 def generate_list():
     alist = [x for x in range(random.randint(-10,10))]
     assert(sum(alist)<100),"sum >100"
-    assert(len(alist)!=0),"sum is null"
+    assert(len(alist)>0),"sum is null"
     return alist
 
 def printIT():
--- src/genlist_test.py
+++ src/genlist_test.py
@@ -2,6 +2,7 @@ import sys
 import os
 cwd = os.getcwd()
 sys.path.append(cwd)
-from generate_list import printIT
 
-printIT()
+from generate_list import printIT
+for num in range(0,1000):
+    printIT()
